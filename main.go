5ca74bcb197ba93deeaaffb9540b5990016ee982
Johannes Staffans <johannes.staffans@gmail.com>
2021-11-25 10:42:42 +0200

    Add healthcheck endpoint

--- main.go
+++ main.go
@@ -10,8 +10,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello there!")
 }
 
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK")
+}
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/healthcheck", healthCheckHandler)
 	log.Print("HTTP server listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
